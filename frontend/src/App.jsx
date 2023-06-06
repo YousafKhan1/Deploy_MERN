133bab426d95f706206eb471e5cac4e44ab7880d
YousafKhan1 <53962585+YousafKhan1@users.noreply.github.com>
2023-06-06 10:47:28 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 axios.defaults.withCredentials = true;
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('https://deploy-mern-5wsr.vercel.app/register', {name, email, password})
+    axios.post('https://deploy-mern-api.vercel.app/register', {name, email, password})
     .then(result => console.log(result))
     .catch(err => console.log(err))
   }
