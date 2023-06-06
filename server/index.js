5a081c7e44c4543503190bb1528c4c1dda1ec7b7
YousafKhan1 <53962585+YousafKhan1@users.noreply.github.com>
2023-06-06 09:55:23 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors(
 ));
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority');
 
 
 app.get("/", (req, res) => {
